189d189e81e0ed6553c527a0df7dde59b3e4dfb7
Daniel Salvagni <daniel.salvagni@senior.com.br>
2016-07-28 18:40:39 -0300

    Testing commander as a CLI interface

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-var action   = require('./lib/actions');
+var cli      = require('./lib/cli'),
+    program  = require('commander'),
+    pkg      = require('./package.json');
+
+
+
+program
+  .version(pkg.version);
+
+cli.registerAll(program);
+
+program.parse(process.argv);
