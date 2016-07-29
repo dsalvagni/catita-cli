58544f94419ae59c50cf06463eca52bb4f3e89d3
Daniel Salvagni <daniel.salvagni@senior.com.br>
2016-07-29 15:27:46 -0300

    Organize actions in separated files I think its better to work with separation of concerns, so I split the command line actions code to one file each. Also, I'd made easy to register new actions by loading all `.js` files from the actions folder.
    All those files has the same code structure, which makes it easy to
    maintain and understand.
    
    Add a `config.json` files with API URL and endpoint URLs configuration.

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var cli      = require('./lib/cli'),
     program  = require('commander'),
     pkg      = require('./package.json');
 
-
-
 program
   .version(pkg.version);
-
-cli.registerAll(program);
 
 program.parse(process.argv);
